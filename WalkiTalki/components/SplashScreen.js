4f21cedf3a49fa83a941726f5157d92aa286f132
Vismita Uppalli <vu8hh@virginia.edu>
2019-11-11 13:19:00 -0500

    SplashScreen with background

--- WalkiTalki/components/SplashScreen.js
+++ WalkiTalki/components/SplashScreen.js
@@ -1,34 +1,32 @@
 import * as React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function SplashScreen() {
     return (
-        <View style={styles.container}> 
-            <Image source={require('../assets/WalkiTalkiLogo.png')}/>
-            <Text style={styles.words}>Walki Talki</Text>
-        </View>
-    )
+        <LinearGradient 
+          start={[1,1]}
+          end={[0.4, 0.3]}
+            colors={[ '#FF70A6', '#4BB3FD',]} 
+            style={styles.gradientStyles}>
+          <Image source={require('../assets/WalkiTalkiLogo.png')}/>
+          <Text style={styles.logo}> Walki Talki </Text>
+        </LinearGradient> 
+    )   
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
+    gradientStyles:{
+     flex: 1,
       flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
-      backgroundColor: 'blue',
     },
-    picture: {
-      width: 211.89,
-      height: 192,
-      marginBottom: 25,
+    logo:{
+      color: 'white',
+      fontWeight: 'bold',
+      fontSize: 30,
+      height:100,
+      paddingTop: 30,
     },
-    words: {
-      fontFamily: 'Helvetica',
-      fontStyle: 'normal',
-      fontWeight: 'normal',
-      fontSize: 65,
-      lineHeight: 78,
-      color: 'white'
-    }
-  });
+  })
