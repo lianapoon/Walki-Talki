5c2145b9610aef42efa1247cdf725ecf21119277
Sharon Bryant <sharonbryant@Sharons-MacBook-Pro.local>
2019-11-16 23:42:24 -0500

    Added some chat screen example code using gifted chat. Also added keyboard spacer lib

--- WalkiTalki/components/ChatScreen.js
+++ WalkiTalki/components/ChatScreen.js
@@ -1,21 +1,54 @@
 import React from 'react';
 import { View, Text, StyleSheet} from 'react-native';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
 import { GiftedChat } from 'react-native-gifted-chat'
 
 export default class ChatScreen extends React.Component{
-    render() {
+    state = {
+        messages: [],
+      }
+    
+      componentWillMount() {
+        this.setState({
+          messages: [
+            {
+              _id: 1,
+              text: 'Hello developer',
+              createdAt: new Date(),
+              user: {
+                _id: 2,
+                name: 'React Native',
+                avatar: 'https://placeimg.com/140/140/any',
+              },
+            },
+          ],
+        })
+      }
+    
+      onSend(messages = []) {
+        this.setState(previousState => ({
+          messages: GiftedChat.append(previousState.messages, messages),
+        }))
+      }
+    
+      render() {
         return (
-            <View style = {styles.container}>
-                <Text>chat screen!</Text>
-            </View>
+        <View style = {styles.container}>
+          <GiftedChat
+            messages={this.state.messages}
+            onSend={messages => this.onSend(messages)}
+            user={{
+              _id: 1,
+            }}
+          />
+          <KeyboardSpacer/>
+          </View>
         )
+      }
     }
-}
 
 const styles = StyleSheet.create({
     container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center'
+        flex:1
     }
 })
